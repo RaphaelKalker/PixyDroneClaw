bcf144f52e912029f22b8b4d377052504aeca45a
Raphael Kalker <raphael.kalker@gmail.com>
2015-03-01 22:29:44 -0500

    because I want proper identation spacing

--- pixyduino/Claw.cpp
+++ pixyduino/Claw.cpp
@@ -13,7 +13,7 @@
 		*/
 
 		servo1.attach(pin9);
-                servo2.begin(pin2);
+        servo2.begin(pin2);
 		servo3.attach(pin10);
 	}
 
@@ -77,14 +77,14 @@
 			return true;
 		}
 
-                //claw servo
-                servo1.write(position3);
+        //claw servo
+        servo1.write(position3);
 
-                //spyder servo
-                servo2.rotate(spyderClose);
+        //spyder servo
+        servo2.rotate(spyderClose);
 
-                //jordan servo
-                servo3.write(jordanClose);
+        //jordan servo
+        servo3.write(jordanClose);
 
 		Serial.println("Closing claw...");
 		state = CLOSING;
