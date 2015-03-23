30fb1fd17fadfe810b8e50b97717aabb6593ab86
Raphael Kalker <raphael.kalker@gmail.com>
2015-03-22 22:42:20 -0400

    Detection cleanup

--- pixyduino/Detection.cpp
+++ pixyduino/Detection.cpp
@@ -8,59 +8,20 @@ boolean Detection::isObjectCentered(Block block) {
 	* check if item is centered based on the some target thresholds for the object's size
 	* this should ignore false positives for objects of the wrong size
 	*play around with the target values
+	* block.x = CENTER X
+	* block.y = CENTER Y
+	*servo code for robot use 
+	//X_CENTER - pixy.blocks[trackedBlock].x;
+	//pixy.blocks[trackedBlock].y - Y_CENTER
 	*/
 
-	if (block.x > TARGET_X1 && block.x < TARGET_X1 + block.width
-		&& block.y > TARGET_Y1 && block.y < TARGET_Y1 + block.height) {
+	if (block.x > TARGET_X1 && block.x < TARGET_X1 + block.width*SCALING_FACTOR +10
+		&& block.y > TARGET_Y1 && block.y < TARGET_Y1 + block.height*SCALING_FACTOR +10) {
 		char buffalo[128];
-		sprintf(buffalo, "Object is centered -> Sig: %d, xLeft: %d, xRight: %d, yTop: %d, yBottom: %d",
+		sprintf(buffalo, "Object is centered -> Sig: %d, xCenter: %d, xRight: %d, yTop: %d, yBottom: %d",
 			block.signature, block.x, block.x + block.width, block.y, block.y + block.height);
 		Serial.println(buffalo);
 		return true;
 	}
 	return false;
-	//  return block.width > targetWidth || block.height > targetHeight;
 }
-//
-//	void Detection::handlePixyBlocks() {
-//
-//		static int i = 0;
-//		blocks = pixy.getBlocks();
-//		len = sizeof(blocks);
-//		char buf[32];
-//
-//		if (blocks && claw.isReady()) {
-//			if (++i % frames == 0) {
-//				sprintf(buf, "Detected %d objects: \n", blocks);
-//				Serial.print(buf);
-//
-//				//Print Informations about detected objects
-//				for (int j = 0; j < blocks; j++) {
-//					sprintf(buf, " block %d: ", j);
-//					Serial.print(buf);
-//					pixy.blocks[j].print();
-//
-//					//confirm target based on some thresholds
-//					if (Detection::isObjectInRange(pixy.blocks[j])) {
-//						Serial.println("Object is in range");
-//						claw.engage();
-//						Serial.println("done");
-//					}
-//				}
-//			}
-//		} else {
-//
-//
-//		}
-//	}
-//
-boolean Detection::handleBtnPressed(uint8_t btnPin) {
-//	if (digitalRead(btnPin) == HIGH) {
-//		return true;
-//	}
-//	return false;
-	return digitalRead(btnPin) == HIGH;
-}
-//
-//
-//
--- pixyduino/Detection.h
+++ pixyduino/Detection.h
@@ -4,11 +4,6 @@
 #if defined(ARDUINO) && ARDUINO >= 100
 	#include "Arduino.h"
 	#include <Pixy.h>
-	#include <SPI.h>
-	#include <Servo.h>
-	#include "Signature.h"
-	#include "Claw.h"
-	#include "Detection.h"
 #else
 	#include "WProgram.h"
 #endif
@@ -17,24 +12,14 @@ class Detection {
 	protected:
 	static const uint8_t targetWidth = 150;
 	static const uint8_t targetHeight = 150;
-	static const uint8_t TARGET_X1 = 150;
+	static const uint8_t TARGET_X1 = 160;
 	static const uint8_t TARGET_X2 = 300;
 	static const uint8_t TARGET_Y1 = 100;
 	static const uint8_t TARGET_Y2 = 300;
-	Pixy pixy;
-//	static const int timer = 1000;
-//	static const uint8_t frames = 25;
-//	static uint16_t blocks;
-//	static uint8_t len;
-//	static Pixy pixy;
-//	static Claw claw;
-//	static Servo servo;
+	static const float SCALING_FACTOR = 1.0;
 
 	public:
 	static boolean isObjectCentered(Block block);
-//	static void handlePixyBlocks();
-	static boolean handleBtnPressed(uint8_t btnPin);
-	void handlePixy();
 	static void init();
 };
 
